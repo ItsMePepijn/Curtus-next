6d5b71dfd30dbc9c37fc5e555b9355bf19cdecbb
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-09-01 22:03:00 +0200

    added error if invalid key

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -12,7 +12,8 @@ export default function handler(req, res) {
       const ID = generateId(5);
       if(key === "USR_") return res.status(201).json({ ID: ID, type: "Anonymous user", generatedLink: `https://dummy-site.com/${ID}`, originalAddress: "https://waylongerdummy-site.com/this-is-way-too-long"});
     }
-    
+    else return res.status(401).json({ error: "Invalid API key" });
   }
+
   else return res.status(405).json({ error: "Method not allowed, POST must be used for this endpoint" });
 }
