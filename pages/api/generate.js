de75c67f5127ca2963d3e6caefa0b557738ff9d3
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-09-01 20:31:48 +0200

    first generate method

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -3,8 +3,16 @@ import generateId from "../../modules/generateId"
 
 export default function handler(req, res) {
 
-  if(req.method === "POST"){
-    res.status(200).json({ id: generateId(5) })
+  if(req.method === "POST"){ 
+    const { key } = req.body;
+
+    if(!key) return res.status(400).json({ error: "Missing API key"});
+
+    if(key.startsWith("USR_")){
+      const ID = generateId(5);
+      if(key === "USR_") return res.status(201).json({ ID: ID, type: "Anonymous user", generatedLink: `https://dummy-site.com/${ID}`, originalAddress: "https://waylongerdummy-site.com/this-is-way-too-long"});
+    }
+    
   }
 
 }
