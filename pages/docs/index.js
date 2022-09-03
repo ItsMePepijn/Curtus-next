c2f15233670224d65d99779f8b9dfd3ee219ba15
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-09-03 14:41:39 +0200

    created api pages

---
+++ pages/docs/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import styles from '../../styles/Home.module.scss'
+
+export default function Api() {
+  return(
+    <div className={styles.container}>
+      <Head>
+        <title>Link shortener</title>
+        <meta name="description" content="THE link shortening solution for creating your short links made easy and safe!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to the link api docs!
+        </h1>
+      </main>
+
+    </div>
+  )
+}
