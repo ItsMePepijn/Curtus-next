27ab93289842271886074891e2a13db7578a3a20
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-09-03 12:34:18 +0200

    api page at /api

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api',
+        destination: '/apiInfo',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
