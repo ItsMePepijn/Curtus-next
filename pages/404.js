e6f70f04cad96694aa72a8fddffd20ac2e0cae1d
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-09-03 21:38:57 +0200

    added head to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,16 @@
+import Head from "next/head"
+import styles from '../styles/Home.module.scss'
+
 export default function Error404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>404: This page does not exist</title>
+        <meta name="description" content="This page does not exist!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <h1>404 - Page Not Found</h1>
+    </div>
+  )
 }
