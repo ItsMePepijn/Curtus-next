a6facff92cf593144e762a9330de3c428bfab67a
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-09-03 22:58:47 +0200

    functioning database (needs work)

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,19 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import generateId from "../../modules/generateId"
 
-export default function handler(req, res) {
+const { connectToDatabase } = require('../../modules/mongodb');
 
-  if(req.method === "POST"){ 
-    const { key } = req.body;
+export default async function handler(req, res) {
 
-    if(!key) return res.status(400).json({ error: "Missing API key"});
+  if(req.method !== "POST") return res.status(405).json({ error: "Method not allowed, POST must be used for this endpoint", usedMethod: req.method });
 
-    if(key.startsWith("USR_")){
-      const ID = generateId(5);
-      if(key === "USR_") return res.status(201).json({ ID: ID, type: "Anonymous user", generatedLink: `https://dummy-site.com/${ID}`, originalAddress: "https://waylongerdummy-site.com/this-is-way-too-long"});
+  const { key, url } = req.body;
+
+  if(!key) return res.status(400).json({ error: "Missing API key"});
+  if(!url) return res.status(400).json({ error: "Missing URL"});
+
+  if(key.startsWith("USR_")){
+    const ID = generateId(5);
+
+    try{
+      let { db } = await connectToDatabase();
+      await db.collection("urls").insertOne({
+        _id: ID,
+        generatedBy: key,
+        url: url
+      })
+
+      const user = (key === "USR_") ? "Anonymous user" : "User"
+    
+      return res.status(201).json({ ID: ID, type: user, generatedLink: `https://dummy-site.com/${ID}`, originalAddress: url});
+
+    } 
+    catch(e){
+      return res.status(500).json({ error: "Internal server error" });
     }
-    else return res.status(401).json({ error: "Invalid API key" });
+
   }
 
-  else return res.status(405).json({ error: "Method not allowed, POST must be used for this endpoint", usedMethod: req.method });
+  else return res.status(401).json({ error: "Invalid API key" });
 }
