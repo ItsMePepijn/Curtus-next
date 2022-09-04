c9d1b500efa9f0d86e0873387708009852c293d4
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-09-04 19:32:59 +0200

    navbar component

---
+++ components/navBar.js
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import favicon from "../public/favicon.ico"
+
+import styles from "../styles/NavBar.module.scss"
+
+export default function NavBar(){
+  return(
+    <nav className={styles.navBar}>
+      <Link href={"/"}><a className={styles.navBarLogo}><Image src={favicon} alt="Logo" width={40} height={40}/></a></Link>
+
+      <div className={styles.navBarLinks}>
+        <Link href={"/"}><a className={styles.navBarLinksActive}>Home</a></Link>
+        <Link href={"/docs"}><a>Docs</a></Link>
+        <Link href={"/apiInfo"}><a>API</a></Link>
+      </div>
+
+      <Link href={""}><a className={styles.navBarLogin}>Login</a></Link>
+    </nav>
+  )
+}
