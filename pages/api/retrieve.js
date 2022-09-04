9997aa0ca6e65e59b2ed4ef08e12f2f62e4566ec
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-09-04 10:17:09 +0200

    removed body from get request

--- pages/api/retrieve.js
+++ pages/api/retrieve.js
@@ -1,3 +1,4 @@
+import {useRouter} from 'next/router';
 
 const { connectToDatabase } = require('../../modules/mongodb');
 
@@ -5,28 +6,21 @@ export default async function handler(req, res) {
 
   if(req.method !== "GET") return res.status(405).json({ error: "Method not allowed, GET must be used for this endpoint", usedMethod: req.method });
 
-  const { key, id } = req.body;
+  const { id } = req.query;
 
-  if(!key) return res.status(400).json({ error: "Missing API key"});
   if(!id) return res.status(400).json({ error: "Missing ID"});
 
-  if(key.startsWith("USR_")){
+  try{
+    let { db } = await connectToDatabase();
+    let r = await db.collection("urls").find({ _id: id }).toArray();
 
-    try{
-      let { db } = await connectToDatabase();
-      let r = await db.collection("urls").find({ _id: id }).toArray();
+    if(r.length === 0) return res.status(404).json({ error: "ID not found" });
+    const { _id , url } = r[0];      
 
-      if(r.length === 0) return res.status(404).json({ error: "ID not found" });
-      const { _id , url } = r[0];      
+    return res.status(201).json({ ID: _id, originalAddress: url});
 
-      return res.status(201).json({ ID: _id, originalAddress: url});
-
-    } 
-    catch(e){
-      return res.status(500).json({ error: "Internal server error" });
-    }
-
+  } 
+  catch(e){
+    return res.status(500).json({ error: "Internal server error" });
   }
-
-  else return res.status(401).json({ error: "Invalid API key" });
 }
