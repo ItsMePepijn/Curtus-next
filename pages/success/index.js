ebbead0e2e2412db3c4fb9d3a84565f83d6d0e0d
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-09-28 09:44:32 +0200

    better styling

--- pages/success/index.js
+++ pages/success/index.js
@@ -1,7 +1,10 @@
 import { useRouter } from "next/router"
+import Head from "next/head"
 
 import NavBar from "../../components/navBar"
 import Error404 from "../404"
+
+import styles from "../../styles/Home.module.scss"
 
 export default function Redirect(){
   const router = useRouter()
@@ -9,10 +12,36 @@ export default function Redirect(){
 
   const re = /[A-Z]{5}/
   if(!re.test(id)) return <Error404 />
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText("https://curtus.tech/" + id)
+  }
   return(
-    <div>
+    <div className={styles.container}>
+      <Head>
+        <title>Curtus | Generate</title>
+        <meta name="description" content="THE link shortening solution for creating your short links made easy and safe!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <NavBar />
-      <p>{id}</p>
+      
+      <main className={styles.main}>
+        <div className={styles.box}>
+          <h1 className={styles.subTitle}>
+            Your short link is ready!
+          </h1>
+          <p className={styles.text}>https://curtus.tech/{id}</p>
+          
+          <div className={styles.buttons}>
+            <a href={id} className={styles.button}>Bring me there</a>
+            <a className={styles.button} onClick={copyToClipboard}>
+              Copy
+            </a>
+          </div>
+        </div>
+
+      </main>
     </div>
   )
 
