4a349cfe3ac43668db6ca04c4cc43f72df534ee0
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-10-03 20:19:13 +0200

    exactly what it is

---
+++ modules/validateUrl.js
@@ -0,0 +1,4 @@
+export default function validateUrl(url) {
+  const re = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm
+  return re.test(url)
+}
