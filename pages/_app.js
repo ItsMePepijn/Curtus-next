9f33b71bd1761074a7251890cac555ae8957d613
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-10-03 20:38:48 +0200

    loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.scss'
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress color="#ffa500"/>
+      <Component {...pageProps} />;
+    </>
+  )
 }
 
 export default MyApp
