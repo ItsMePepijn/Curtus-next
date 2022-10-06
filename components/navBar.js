f8315239ac71704391185977090fd15491651df9
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-10-06 14:43:14 +0200

    better routing, selected page color now works

--- components/navBar.js
+++ components/navBar.js
@@ -1,22 +1,47 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router";
 
 import favicon from "../public/favicon.ico"
 
 import styles from "../styles/NavBar.module.scss"
 
+const navigationRoutes = ["Home", "Generate", "Docs", "API"]
+
 export default function NavBar(){
+  const router = useRouter();
   return(
     <nav className={styles.navBar}>
       <Link href={"/"}><a className={styles.navBarLogo}><Image src={favicon} alt="Logo" width={40} height={40}/></a></Link>
 
       <div className={styles.navBarLinks}>
-        <Link href={"/"}><a className={styles.navBarLinksActive}>Home</a></Link>
-        <Link href={"/docs"}><a>Docs</a></Link>
-        <Link href={"/apiInfo"}><a>API</a></Link>
+        {navigationRoutes.map((route) => {
+          var name = route;
+
+          route = route == "Home" ? "" : route;
+          route = route == "API" ? "apiInfo" : route;
+          return(
+            <NavigationLink 
+              key={route}
+              name={name}
+              route={route.toLowerCase() == "apiinfo" ? "api" : route.toLowerCase()}
+              cls = {router.pathname.toLowerCase() === `/${route.toLowerCase()}` ? styles.navBarLinksActive : styles.navBarLinks}
+            />
+          )
+
+        })}
+
       </div>
 
       <Link href={"/login"}><a className={styles.navBarLogin}>Login</a></Link>
     </nav>
+  )
+}
+
+function NavigationLink({name, route, cls}){
+  return(
+    <Link href={`/${route}`}>
+      <a className={cls}>{name}</a>
+    </Link>
   )
 }
