6936244da9cbeaa9a6fe2795cb501de5cb5a8124
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-10-13 09:30:42 +0200

    docs base

---
+++ components/DocNav.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { useRouter } from "next/router";
+
+import styles from '../styles/Docs.module.scss'
+
+export default function DocNav({ver, endpoints, className}) {
+  const router = useRouter();
+  return (
+    <div className={className}>
+      <p className={router.pathname.toLowerCase() == `/docs/${ver}` ? styles.titleActive : styles.title}><Link href={`/docs/${ver}`}><a>Welcome</a></Link></p>
+
+      {endpoints.map((endpoint) => (
+        <NavigationLink 
+          key={endpoint}
+          endpoint={endpoint}
+          ver={ver}
+          cls={router.pathname.toLowerCase() == `/docs/${ver}/${endpoint}` ? styles.titleActive : styles.title}
+        />
+      ))}
+    </div>
+  )
+}
+
+function NavigationLink({endpoint, ver, cls}){
+  const name = endpoint.charAt(0).toUpperCase() + endpoint.slice(1);
+
+  return(
+    <p className={cls}>
+      <Link href={`/docs/${ver}/${endpoint}`}><a>{name}</a></Link>
+    </p>
+  )
+}
---
+++ pages/docs/v0/index.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+
+import styles from '../../../styles/Docs.module.scss'
+
+import * as docs from '../../api/v0/docs/index'
+import DocNav from '../../../components/DocNav'
+
+export default function Api() {
+  var endpoints = Object.keys(docs);
+  return(
+    <div>
+      <Head>
+        <title>Curtus | v0 Documentation</title>
+        <meta name="description" content="THE link shortening solution for creating your short links made easy and safe!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="main">
+        <div className={styles.boxWide}>
+          <DocNav ver="v0" endpoints={endpoints} className={styles.left}/>
+          <div className={styles.right}>
+            <h1 className="subTitle">
+              Welcome to the v0 api docs!
+            </h1>
+          </div>
+          
+        </div>
+
+      </main>
+
+    </div>
+  )
+}
