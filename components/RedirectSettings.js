27a510fd1eac4c26632bc3184a3be7beb31b29fc
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-10-17 19:31:38 +0200

    better animations (first load state broken still)

--- components/RedirectSettings.js
+++ components/RedirectSettings.js
@@ -1,5 +1,5 @@
 import {setCookie, getCookie} from 'cookies-next'
-import { useEffect, useState } from 'react'
+import { createRef, useState } from 'react'
 
 import styles from '../styles/RedirectSettings.module.scss'
 
@@ -29,7 +29,7 @@ export default function Settings({data}){
 
   function changeDelayTime(e) {
     setDelayTime(e.target.value);
-    e.target.value = (e.target.value <= 0) ? e.target.value = 0 : e.target.value;
+    e.target.value = (!e.target.value > 0) ? e.target.value = 0 : e.target.value;
 
     const cookies = new cookie.Settings(isEnabled, isDelayed, parseInt(e.target.value));
     setCookie("redirectSettings", cookies, cookies.rules());
@@ -37,11 +37,31 @@ export default function Settings({data}){
     console.log(JSON.parse(getCookie("redirectSettings")));
   }
 
+  let cls
+  let rowHidden
+  let rowHidden2
+
+  if(!isEnabled){
+    cls = styles.settingsContainer + " " + styles.settingsContainerHidden
+    rowHidden = styles.rowHidden
+    rowHidden2 = styles.rowHidden
+  }
+  else if(!isDelayed){
+    cls = styles.settingsContainer + " " + styles.settingsContainerHidden2
+    rowHidden = ""
+    rowHidden2 = styles.rowHidden
+  }
+  else{
+    cls = styles.settingsContainer
+    rowHidden = ""
+    rowHidden2 = ""
+  }
+
   return(
-    <div className={styles.settingsContainer}>
-      <form>
+    <div className={cls}>
+      <form onSubmit={(e) => {e.preventDefault()}}>
         <table>
-          <tbody>
+          <tbody className={styles.tableBody}>
             <tr>
               <td className={styles.label}>Disable redirect confirmation</td>
               <td>
@@ -52,7 +72,7 @@ export default function Settings({data}){
               </td>
             </tr>
 
-            <tr>
+            <tr className={rowHidden}>
               <td className={styles.label}>Enable redirect delay</td>
               <td>
                 <label className={styles.switch}>
@@ -62,7 +82,7 @@ export default function Settings({data}){
               </td>
             </tr>
 
-            <tr>
+            <tr className={rowHidden2}>
               <td className={styles.label}>Redirect delay</td>
               <td>
                 <label className={styles.num}>
