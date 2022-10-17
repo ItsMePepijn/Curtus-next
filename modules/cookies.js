b0250d94631750352e38a2cc6491ead96d5c314b
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-10-17 16:52:17 +0200

    part of previous commit

--- modules/cookies.js
+++ modules/cookies.js
@@ -1,3 +1,4 @@
+import { setCookie, getCookie, hasCookie } from 'cookies-next'
 
 export class Settings{
   constructor(isEnabled = false, isDelayed = false, delayTime = 0){
@@ -8,5 +9,23 @@ export class Settings{
   
   rules(){
     return {path: "/", maxAge: 1 * 60 * 60 * 24 * 365, sameSite: true};  
+  }
+}
+
+export function get(cookie){
+  if(cookie == "redirectSettings"){
+
+    if(typeof window == 'undefined') return new Settings();
+
+    else{
+      if(hasCookie(cookie)) return JSON.parse(getCookie(cookie));
+
+      else{
+        const cookies = new Settings();
+        setCookie(cookie, cookies, cookies.rules());
+        return cookies;
+      }
+    }
+
   }
 }
