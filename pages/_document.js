105a6721a50cfbe9d390bb970becb577cecdfce8
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-10-21 11:19:29 +0200

    font change and slight design change

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head >
+        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
